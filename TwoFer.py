f2a25ecba3f089c58f3a7d53b2ef45c5e6a553c3
thalimoura <165019843+thalimoura@users.noreply.github.com>
2024-03-26 09:09:54 -0300

    Add files via upload

---
+++ TwoFer.py
@@ -0,0 +1,13 @@
+print("TwoFer")
+
+def grande():
+    nome = input("informe seu nome:")
+
+    if not nome:
+     nome = ("você")
+    else:
+        pass
+
+    print(f"uma bala para {nome}, uma bala para mim")
+
+grande()
